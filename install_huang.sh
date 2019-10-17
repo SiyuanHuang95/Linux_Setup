074f44c0378826ba0a1078311460a8722a417df8
Siyuan <siyuan@siyuan.surface>
2019-10-17 11:51:19 +0200

    fix some bugs

--- install_huang.sh
+++ install_huang.sh
@@ -2,30 +2,56 @@ wget -qO- https://github.com/retorquere/zotero-deb/releases/download/apt-get/ins
 
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt-get update
-sudo apt-get upgrade -y
+sudo apt-get upgrade
 
-sudo apt-get install libeigen3-dev -y
+sudo apt-get install libeigen3-dev 
 sudo cp -r /usr/local/include/eigen3/Eigen /usr/local/include
 
 
-sudo apt-get install ros-kinetic-opencv3 libglew-dev libgoogle-glog-dev libatlas-base-dev libeigen3-dev libsuitesparse-dev libboost-dev libboost-filesystem-dev libopencv-dev -y
+sudo apt-get install ros-kinetic-opencv3 libglew-dev libgoogle-glog-dev libatlas-base-dev libeigen3-dev libsuitesparse-dev libboost-dev libboost-filesystem-dev libopencv-dev 
 
 # terminal emulator YAKUAKE
-sudo apt-get install yakuake -y
+sudo apt-get install yakuake 
 
 # markdown editor
 wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
 sudo add-apt-repository 'deb https://typora.io/linux ./'
-sudo apt-get install typora -y
+sudo apt-get install typora 
 
 # literature
-sudo apt install zotero -y
+sudo apt install zotero 
 
 # ceres
-sudo apt-get install cmake libgoogle-glog-dev libatlas-base-dev libeigen3-dev
+
+git clone https://ceres-solver.googlesource.com/ceres-solver
+# CMake
+sudo apt-get install cmake
+# google-glog + gflags
+sudo apt-get install libgoogle-glog-dev
+# BLAS & LAPACK
+sudo apt-get install libatlas-base-dev
+# Eigen3
+sudo apt-get install libeigen3-dev
+# SuiteSparse and CXSparse (optional)
+# - If you want to build Ceres as a *static* library (the default)
+#   you can use the SuiteSparse package in the main Ubuntu package
+#   repository:
+sudo apt-get install libsuitesparse-dev
+# - However, if you want to build Ceres as a *shared* library, you must
+#   add the following PPA:
 sudo add-apt-repository ppa:bzindovic/suitesparse-bugfix-1319687
 sudo apt-get update
-sudo apt-get install libsuitesparse-dev -y
+sudo apt-get install libsuitesparse-dev
+git clone https://ceres-solver.googlesource.com/ceres-solver
+cd ceres-bin
+cmake ../ceres-solver-1.14.0
+make -j3
+make test
+# Optionally install Ceres, it can also be exported using CMake which
+# allows Ceres to be used without requiring installation, see the documentation
+# for the EXPORT_BUILD_DIR option for more information.
+make install
+
 
 # ROS
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
