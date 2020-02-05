9389bc1af55b83e2fcde7be487c1751857a07a21
AnfangRobkit <siyuan.huang@student.kit.edu>
2020-02-06 00:07:53 +0100

    add code install and backup bashrc

--- install_huang.sh
+++ install_huang.sh
@@ -63,3 +63,15 @@ rosdep update
 echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential
+
+# Code
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install code # or code-insiders
+
+# cLion
+sudo apt install cmake gcc g++ clang
+
