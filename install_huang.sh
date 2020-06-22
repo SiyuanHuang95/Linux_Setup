3e063a31459d39d8ee3c3419ec622b6159b93c99
Siyuan Huang <siyuan.huang@student.kit.edu>
2020-06-22 02:05:42 +0200

    add jekyll for personal website

--- install_huang.sh
+++ install_huang.sh
@@ -88,4 +88,10 @@ sudo apt-get install python-catkin-tools
 # htop
 sudo apt install htop
 
-
+# jekyll for personal website
+sudo apt-get install ruby-full build-essential zlib1g-dev
+echo '# Install Ruby Gems to ~/gems' >> ~/.bashrc
+echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
+echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+gem install jekyll bundler
